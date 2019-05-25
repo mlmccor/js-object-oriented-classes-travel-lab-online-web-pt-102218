a418cdd09b74c058197282352db2d195b93eaa0f
Matthew McCormack <mmccormack1182@gmail.com>
2019-05-25 19:52:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+class Driver {
+  constructor(name, dateString) {
+    this.name = name
+    this.dateString = dateString
+  }
+}
 
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+}
