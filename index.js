93eda6fbd1c575c0ba17e4822b8d67e5c3af8844
Matthew McCormack <mmccormack1182@gmail.com>
2019-05-28 12:50:32 -0400

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 class Driver {
   constructor(name, dateString) {
     this.name = name
-    this.startDate = new Date(dateString)
+    let dateArray = dateString.split(" ")
+    this.startDate = new Date(parseInt(dateArray[2]), 0)
   }
-  
+
   yearsExperienceFromBeginningOf(year) {
-    
+    let date = new Date(year, 0)
+    return (date.getFullYear() - this.startDate.getFullYear())
   }
-  
+
 }
 
 
@@ -16,4 +18,29 @@ class Route {
     this.beginningLocation = beginningLocation
     this.endingLocation = endingLocation
   }
+
+  blocksTravelled() {
+    let eastWest = [
+      '1st Avenue',
+      '2nd Avenue',
+      '3rd Avenue',
+      'Lexington Avenue',
+      'Park',
+      'Madison Avenue',
+      '5th Avenue'
+    ];
+    let horizontalTraveled = Math.abs(eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endingLocation.horizontal))
+    let verticalTraveled = Math.abs(parseInt(this.beginningLocation.vertical)-parseInt(this.endingLocation.vertical))
+    return horizontalTraveled + verticalTraveled
+  }
+
+  estimatedTime(peak) {
+    if (peak) {
+      return this.blocksTravelled() / 2
+    } else {
+      return this.blocksTravelled() / 3
+    }
+  }
+
+
 }
